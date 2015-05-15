19b022ac4b645fb4be0cee65934ac81124a55fd0
Uice Lu <uicestone@gmail.com>
2015-05-15 14:07:36 +0800

    format uploaded image filename

--- wp-content/themes/landmarkphoto/functions.php
+++ wp-content/themes/landmarkphoto/functions.php
@@ -6,3 +6,15 @@ add_action('wp_enqueue_scripts', function(){
 	wp_enqueue_style('style');
 	wp_enqueue_script('jquery');
 });
+
+add_action('sanitize_file_name', function($filename){
+
+	$ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+	if(isset($_POST['attendee_name']) && isset($_POST['phone'])){
+		return $_POST['attendee_name'] . '-' . $_POST['phone'] . '.' . $ext;
+	}
+
+	return $filename;
+	
+}, 10);
